a1b77ceb7c17bffc7db512292d473c02c0e98a6c
Shubham Sahu <ss.cse1001@gmail.com>
2023-05-31 23:39:12 +0530

    fix options

--- src/Utils/data.ts
+++ src/Utils/data.ts
@@ -1,70 +1,70 @@
 export const zoomOptions = [
-    { name: '100%', href: '#' },
-    { name: '50%', href: '#' },
-    { name: '75%', href: '#' },
-    { name: '90%', href: '#' },
-    { name: '100%', href: '#' },
-    { name: '125%', href: '#' },
-    { name: '150%', href: '#' }
-  ];
+  { name: '100%', href: '#' },
+  { name: '50%', href: '#' },
+  { name: '75%', href: '#' },
+  { name: '90%', href: '#' },
+  { name: '100%', href: '#' },
+  { name: '125%', href: '#' },
+  { name: '150%', href: '#' }
+];
 
 export const textOptions = [
-    {
-      name: 'Normal text',
-      href: '#'
-    },
-    {
-      name: 'Title',
-      href: '#'
-    },
-    {
-      name: 'Heading 1',
-      href: '#'
-    },
-    {
-      name: 'Heading 2',
-      href: '#'
-    },
-    {
-      name: 'Heading 3',
-      href: '#'
-    }
-  ];
+  {
+    name: 'Normal text',
+    href: '#'
+  },
+  {
+    name: 'Title',
+    href: '#'
+  },
+  {
+    name: 'Heading 1',
+    href: '#'
+  },
+  {
+    name: 'Heading 2',
+    href: '#'
+  },
+  {
+    name: 'Heading 3',
+    href: '#'
+  }
+];
 
 export const textFont = [
-    {
-      name: 'Arial',
-      href: '#'
-    },
-    {
-      name: 'Impact',
-      href: '#'
-    },
-    {
-      name: 'Comic Sans MS',
-      href: '#'
-    },
-    {
-      name: 'Generic Sans',
-      href: '#'
-    },
-    {
-      name: 'Lobster',
-      href: '#'
-    }
-  ];
+  {
+    name: 'Arial',
+    href: '#'
+  },
+  {
+    name: 'Impact',
+    href: '#'
+  },
+  {
+    name: 'Comic Sans MS',
+    href: '#'
+  },
+  {
+    name: 'Generic Sans',
+    href: '#'
+  },
+  {
+    name: 'Lobster',
+    href: '#'
+  }
+];
 
 export const documentViewOption = [
-    {
-      name: 'Editing',
-      href: '#'
-    },
-    {
-      name: 'Suggesting',
-      href: '#'
-    },
-    {
-      name: 'Viewing',
-      href: '#'
-    }
-  ];
+  {
+    name: 'Editing',
+    href: '#'
+  },
+  {
+    name: 'Suggesting',
+    href: '#'
+  },
+  {
+    name: 'Viewing',
+    href: '#'
+  }
+];
